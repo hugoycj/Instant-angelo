9f682a1b8292387689742654350a8f1ce5dfe258
zebin-dm <zebin.cai@deepmirror.com>
2023-12-06 19:50:45 +0800

    update

--- run_neuralangelo-colmap_sparse.sh
+++ run_neuralangelo-colmap_sparse.sh
@@ -6,15 +6,15 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${CUDA_HOME}/lib64:${CUDA_HOME}/extras/C
 export PATH=$PATH:$CUDA_HOME/bin
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 starttime=$(date +%Y-%m-%d\ %H:%M:%S)
-# echo "---sfm---"
-# python scripts/run_colmap.py $INPUT_DIR
-# sfm_time=`date +"%Y-%m-%d %H:%M:%S"`
+echo "---sfm---"
+python scripts/run_colmap.py $INPUT_DIR
+sfm_time=`date +"%Y-%m-%d %H:%M:%S"`
 
-# echo "---sparse_visualize---"
-# colmap model_converter \
-#     --input_path $INPUT_DIR/sparse/0 \
-#     --output_path $INPUT_DIR/sparse/0/points3D.ply --output_type PLY
-# sparse_visualize_time=`date +"%Y-%m-%d %H:%M:%S"`
+echo "---sparse_visualize---"
+colmap model_converter \
+    --input_path $INPUT_DIR/sparse/0 \
+    --output_path $INPUT_DIR/sparse/0/points3D.ply --output_type PLY
+sparse_visualize_time=`date +"%Y-%m-%d %H:%M:%S"`
 
 echo "---angelo_recon---"
 python launch.py --config configs/neuralangelo-colmap_sparse.yaml --gpu 1 --train     dataset.root_dir=$INPUT_DIR
